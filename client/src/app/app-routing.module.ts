6dd5a4fb4d4aaa753b484fd5c97891099bfaae40
evaste <info@evaste.com>
2019-08-22 15:52:24 +0100

    newCommit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,27 +1,26 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-/*import {NavComponent} from './nav/nav.component';*/
-import {PatientsComponent} from './patients/patients.component';
-import {ConsultationFormsPatienComponent} from './consultation-forms-patien/consultation-forms-patien.component'
-import {PharmacyComponent} from './pharmacy/pharmacy.component';
-import {ProfileComponent} from './profile/profile.component';
-// import {PatientListComponent} from './patient-list/patient-list.component';
+// import {NavComponent } from './nav/nav.component';
+import { PatientsComponent } from './patients/patients.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { ConsultationFormsPatienComponent } from './consultation-forms-patien/consultation-forms-patien.component';
+import { PharmacyComponent } from './pharmacy/pharmacy.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
-
-  const  routes: Routes =[
-  {path: 'doctor', component: HomeComponent},
-  {path: 'doctor/patients', component: PatientsComponent},
-  {path: 'doctor/patients/ConsultationFormsPatienComponent' , component: ConsultationFormsPatienComponent},
-  // {path: 'doctor/patients/patientList' , component: PatientListComponent},
-  {path: 'doctor/pharmacy', component: PharmacyComponent},
-  {path: 'doctor/profile', component: ProfileComponent}
+const routes: Routes = [
+  { path: 'doctor', component: HomeComponent },
+  { path: 'doctor/patients', component: PatientsComponent },
+  { path: 'doctor/patients/patientList' , component : PatientListComponent},
+  { path: 'doctor/patients/consultation', component: ConsultationFormsPatienComponent },
+  { path: 'doctor/pharmacy', component: PharmacyComponent },
+  { path: 'docotr/profile', component: ProfileComponent }
 ];
 
 
 @NgModule({
-  imports:[RouterModule.forRoot(routes)],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,35 +1,28 @@
-import { PatientListComponent } from '.';
-// import { PatientListComponent } from './patient-list/patient-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { PatientsComponent } from './patients/patients.component';
 import { NavPatientComponent } from './nav-patient/nav-patient.component';
-// import { PatientListComponent } from './PatientListComponent/PatientListComponent';
-import {PatientListComponent} from '.PatientListComponent/PatientListComponent';
+import { HomeComponent } from './home/home.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { PatientsComponent } from './patients/patients.component';
 import { ConsultationFormsPatienComponent } from './consultation-forms-patien/consultation-forms-patien.component';
 import { PharmacyComponent } from './pharmacy/pharmacy.component';
 import { ProfileComponent } from './profile/profile.component';
-// import { NavPatientComponent } from './nav_p_atient/nav-patient.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent,
-    PatientsComponent,
     NavPatientComponent,
+    HomeComponent,
     PatientListComponent,
+    PatientsComponent,
     ConsultationFormsPatienComponent,
     PharmacyComponent,
     ProfileComponent,
-
   ],
   imports: [
     BrowserModule,
